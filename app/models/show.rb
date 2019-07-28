83d05e8403f26198a3effe70d4a558f05d9da9c0
kevin lang <langkevina@gmail.com>
2019-07-28 20:26:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,10 +8,10 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     binding.pry
     show = Show.order(rating: :desc).limit(1)
-    show
+    show.name
   end
   def self.least_popular_show
     show = Show.order(rating: :asc).limit(1)
-    show
+    show.name
   end
 end
