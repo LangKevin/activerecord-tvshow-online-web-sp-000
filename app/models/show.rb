7b2536e165a2c827bafd5f9c40739f3753ca97eb
kevin lang <langkevina@gmail.com>
2019-07-28 20:47:08 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
     show = Show.order(rating: :desc).limit(1)
     # show.first.attributes["name"]
     binding.pry
-    return show.as_json
+    show
 
   end
   def self.least_popular_show
